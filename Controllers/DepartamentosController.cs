57f7f61a1fda24525b1ba5339d7f52ff7105b230
João Vitor de Oliveira Cara <62843342+joaovcara@users.noreply.github.com>
2021-06-26 15:57:18 -0300

    Model and View Departamentos

---
+++ Controllers/DepartamentosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Models;
+
+namespace VendasWebMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+
+        #region IndexDepartamentos
+        public IActionResult Index()
+        {
+
+            List<Departamento> lista = new List<Departamento>();
+
+            lista.Add(new Departamento { IdDepartamento = 1, Descricao = "Eletronicos" });
+            lista.Add(new Departamento { IdDepartamento = 2, Descricao = "Celulares" });
+
+            return View(lista);
+        }
+        #endregion
+    }
+}
---
+++ Models/Departamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendasWebMVC.Models
+{
+    public class Departamento
+    {
+
+        #region Propriedades
+        public int IdDepartamento { get; set; }
+        public string Descricao { get; set; }
+        #endregion
+
+    }
+}
